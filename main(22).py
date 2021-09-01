299376fb2378c200a34fba59b2d9ca031e4754ef
danielnlz01 <38385574+danielnlz01@users.noreply.github.com>
2021-08-31 21:29:29 -0500

    Add files via upload

---
+++ main(22).py
@@ -0,0 +1,9 @@
+def farenheit(centígrados):
+  fahrenheit_temp=((9/5)*centígrados)+32
+  return fahrenheit_temp
+
+def kelvin(centígrados):
+  kelvin_temp=centígrados+273.15
+  return kelvin_temp
+  
+centígrados=int(input("Dame la temperatura en Celcius"))
